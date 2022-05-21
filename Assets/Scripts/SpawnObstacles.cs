3bb8e1d08a0c7c70a5b38e4113a384effd5ae2cb
Kim Samson <kimsamson2.0@gmail.com>
2022-05-21 11:27:23 +0200

    Update files for object interval spawning

--- Assets/Scripts/SpawnObstacles.cs
+++ Assets/Scripts/SpawnObstacles.cs
@@ -4,39 +4,78 @@ using UnityEngine;
 
 public class SpawnObstacles : MonoBehaviour
 {
-    List<GameObject> ObstacleList = new List<GameObject>();
+    List<GameObject> MindfulObjectList = new List<GameObject>();
     public GameObject obstacle;
-    public GameObject duckObstacle;
-    public float maxX;
-    public float minX;
+    public GameObject InhaleCloud;
+    public GameObject ExhaleCloud;
+    public float xSpawnRange;
+    public float minXSpawnRange;
+    public float ySpawnValue;
     public float timeBetweenSpawn;
-    private float spawnTime;
-
+    public float mindfulSpawnRate;
+    private float beginSpawn = 4.0f;
+    private float spawnRate;
+    // public float spawnDelay;
+    private bool isMindfulnessStageActive = false;
     private void Start()
     {
         LoadObstacles();
+        InvokeRepeating("SpawnMindfulObstacles", beginSpawn, mindfulSpawnRate);
+        //StartCoroutine(SpawnMindfulObjects());
     }
 
 
     void Update()
     {
-        if (Time.time > spawnTime)
+        if (Time.time > spawnRate)
         {
             Spawn();
-            spawnTime = Time.time + timeBetweenSpawn;
+            spawnRate = Time.time + timeBetweenSpawn;
         }
     }
 
     void Spawn()
     {
-        float randomX = Random.Range(minX, maxX);
+        float randomX = Random.Range(minXSpawnRange, xSpawnRange);
 
         Instantiate(obstacle, transform.position + new Vector3(randomX, -4, 0), transform.rotation);
     }
 
+    //IEnumerator SpawnMindfulObjects()
+    //{
+    //    while(isMindfulnessStageActive)
+    //    {
+    //        foreach (var mindfulobject in MindfulObjectList)
+    //        {
+    //            /*instantiate(mindfulobject, new vector3(random.range(-xspawnrange, xspawnrange), yspawnvalue, 0), quaternion.identity);*/
+    //        }
+    //        Instantiate(InhaleCloud, new Vector3(Random.Range(-xSpawnRange, xSpawnRange), ySpawnValue, 0), Quaternion.identity);
+
+
+    //        yield return new WaitForSeconds(mindfulSpawnRate);
+    //    }
+    //}
+
+
+    void SpawnMindfulObstacles()
+    {
+        float randomX = Random.Range(minXSpawnRange, xSpawnRange);
+
+
+        foreach (var mindfulobject in MindfulObjectList)
+        {
+            Instantiate(mindfulobject, transform.position + new Vector3(randomX, 0, 0), transform.rotation);
+        }
+        //Instantiate(mindfulobject, new Vector3(randomX, ySpawnValue, 0), Quaternion.identity);
+        if (isMindfulnessStageActive)
+        {
+            CancelInvoke();
+        }
+    }
+
     void LoadObstacles()
     {
-        ObstacleList.Add(obstacle);
-        ObstacleList.Add(duckObstacle);
+        MindfulObjectList.Add(InhaleCloud);
+        MindfulObjectList.Add(ExhaleCloud);
     }
 }
