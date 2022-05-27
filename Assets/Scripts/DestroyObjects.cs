8185d98c57304308ee36c2a56edabc31fb9c9694
Kim Samson <kimsamson2.0@gmail.com>
2022-05-27 10:02:29 +0200

    Clean up objects once out of camera

--- Assets/Scripts/DestroyObjects.cs
+++ Assets/Scripts/DestroyObjects.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestroyObstacle : MonoBehaviour
+public class DestroyObjects : MonoBehaviour
 {
     // Start is called before the first frame update
     
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Obstacle")
+        if(collision.gameObject.tag == "Obstacle" || collision.gameObject.tag == "Cloud")
         {
             Destroy(collision.gameObject);   
+        }
+        if (collision.gameObject.tag == "MarshmallowPoint")
+        {
+            Destroy(collision.gameObject);
         }
     }
 }
