ce1a3cbc72a8e9242194692976ac2d365c45f044
Kim Samson <kimsamson2.0@gmail.com>
2022-07-20 19:08:29 +0200

    Fix jump animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,47 +1,39 @@
 using UnityEngine;
 public class Player : MonoBehaviour
 {
-    public float playerSpeed;
-    private Rigidbody2D rigidbody;
-    private float MoveSpeed;
-    private float JumpSpeed;
-    private bool IsJumping;
+    [SerializeField]
+    private Rigidbody2D rb;
     public float jumpValue = 25;
     public int MaxJumpCount = 2;
     public Animator Animator;
-
     private int multiJumpCount;
 
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody2D>();
-        IsJumping = false;
+        rb = GetComponent<Rigidbody2D>();
     }
     // Update is called once per frame
     void Update()
     {
-
         if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
         {
             Animator.SetTrigger("IsJumping");
             multiJumpCount++;
             if (multiJumpCount <= MaxJumpCount)
             {
-                rigidbody.velocity = new Vector3(0, jumpValue, 0);
+                rb.AddForce(transform.up * jumpValue, ForceMode2D.Impulse);
             }
         }
 
         if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
         {
-            rigidbody.transform.localScale = new Vector3(0.3f, 0.2f, 1);
+            rb.transform.localScale = new Vector3(0.3f, 0.2f, 1);
         }
         else
         {
-            rigidbody.transform.localScale = new Vector3(0.3f, 0.3f, 1);
+            rb.transform.localScale = new Vector3(0.3f, 0.3f, 1);
         }
     }
-
-   
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.name == "Ground" || collision.gameObject.tag == "Cloud")
